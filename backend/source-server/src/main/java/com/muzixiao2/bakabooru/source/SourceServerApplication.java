3823f5c735faf5784cca3021a8731c3769532b89
MuziXiao2 <suhxhee@qq.com>
2025-05-15 19:45:46 +0800

    准备重构，整合后端服务器

--- backend/source-server/src/main/java/com/muzixiao2/bakabooru/source/SourceServerApplication.java
+++ backend/source-server/src/main/java/com/muzixiao2/bakabooru/source/SourceServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SourceServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SourceServerApplication.class, args);
     }
